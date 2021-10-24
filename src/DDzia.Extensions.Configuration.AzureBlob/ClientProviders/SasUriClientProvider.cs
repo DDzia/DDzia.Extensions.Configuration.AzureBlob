29637e6f68f3600d32f857b8b0999f43c8f9bf07
Dzianis Dziarkach <ddzia@outlook.com>
2021-10-24 11:15:48 +0300

    Added restricted SasUriClientProvider

---
+++ src/DDzia.Extensions.Configuration.AzureBlob/ClientProviders/SasUriClientProvider.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Azure.Storage.Blobs;
+
+namespace DDzia.Extensions.Configuration.AzureBlob.ClientProviders
+{
+    public class SasUriClientProvider : BlobClientProvider
+    {
+        private readonly Uri uri;
+
+        public SasUriClientProvider(Uri uri)
+        {
+            this.uri = uri;
+        }
+
+        public override Task<BlobClient> Get(
+            Uri blobUri,
+            CancellationToken cancellationToken)
+        {
+            if (blobUri == null)
+            {
+                throw new ArgumentNullException(nameof(blobUri));
+            }
+            return Task.FromResult(new BlobContainerClient(this.uri).GetBlobClient(string.Join(string.Empty, blobUri.Segments.Skip(2))));
+        }
+    }
+}
